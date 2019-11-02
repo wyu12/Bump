74d86bcc2ac4ee6db94931396febf951158b5c78
wyu12 <william.yu@duke.edu>
2019-11-02 14:38:47 -0400

    adding navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import HomePage from '../HomePage.jsx';
+import NavBar from '../NavBar.jsx';
 
 function App() {
   return (
