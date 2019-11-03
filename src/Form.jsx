3370ec8b31e38bbdad05420060e1bd244575034a
Justin Wu <justin.wu@duke.edu>
2019-11-02 20:51:03 -0400

    changed the form margins

--- src/Form.jsx
+++ src/Form.jsx
@@ -15,44 +15,46 @@ class UserForm extends Component {
       <>
         <NavBar></NavBar>
         <body>
-        <Form>
-          <Form.Group as={Row} controlId="formHorizontalFirstName">
-            <Form.Label column sm={1}>First Name
+          <div style={{marginLeft:'0.8rem', marginTop: '0.8rem'}}> 
+          <Form>
+          <Form.Group controlId="formHorizontalFirstName">
+            <Form.Label>First Name
             </Form.Label>
-            <Form.Control type="firstname" placeholder="First Name" style={{width: "30%"}} />
+            <Form.Control type="firstname" placeholder="First Name" style={{width: "30%"}}/>
             <Form.Text className="text-muted">
             </Form.Text>
           </Form.Group>
 
-          <Form.Group as={Row} controlId="formLastName">
-            <Form.Label column sm={1}>Last Name</Form.Label>
+          <Form.Group controlId="formLastName">
+            <Form.Label>Last Name</Form.Label>
             <Form.Control type="lastname" placeholder="Last Name" style={{width: "30%"}} />
             <Form.Text className="text-muted">
             </Form.Text>
           </Form.Group>
 
-          <Form.Group as={Row} controlId="formWeeksPregnant">
-            <Form.Label column sm={1}>Weeks Pregnant</Form.Label>
-            <Form.Control type="weeks" placeholder="Enter Weeks Pregnant" style={{width: "30%"}} />
+          <Form.Group controlId="formWeeksPregnant">
+            <Form.Label>Weeks Pregnant</Form.Label>
+            <Form.Control type="weeks" placeholder="Weeks Pregnant" style={{width: "30%"}} />
             <Form.Text className="text-muted">
             </Form.Text>
           </Form.Group>
 
-          <Form.Group as={Row} controlId="formEmail">
-            <Form.Label column sm={1}>Email</Form.Label>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email</Form.Label>
             <Form.Control type="email" placeholder="Email" style={{width: "30%"}} />
             <Form.Text className="text-muted">
             </Form.Text>
           </Form.Group>
 
-          <Form.Group as={Row} controlId="formBasicPassword">
-            <Form.Label column sm={1}>Password</Form.Label>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" style={{width: "30%"}} />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
   </Button>
-        </Form>
+  </Form>
+          </div>
         </body>
         
       </>
